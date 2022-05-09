d77e53d6f727ba1ddd9362a216fa058b34720497
nayanbanskota <39477251+nayanbanskota@users.noreply.github.com>
2022-05-09 11:47:08 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int a){
+    if(a!=0){
+       return a* fact(a-1);
+    }
+    else{
+        return 1;
+    }
+}
+int combin(int n, int r){
+    return fact(n)/(fact(r)*fact(n-r));
+}
+
+int permut(int n, int r){
+    return fact(n)/fact(n-r);
+}
+
+int main()
+{
+ int n,r;
+ cout<<"Enter n: ";
+ cin>> n;
+ cout<<"Enter r: ";
+ cin>> r;
+
+ cout<<"Permutation of " <<n << " and "<< r << "is: " << permut(n,r)<<endl;
+ cout<<"Combination of " <<n << " and "<< r << "is: " << combin(n,r)<<endl;
+
+}
+
+
